95deedde59b02b99d8e74bf921c937fe3ad6ec45
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2024-10-29 14:08:41 +0100

    java code

--- main.js
+++ main.js
@@ -26,7 +26,64 @@ console.log(isValidPassword("12345678", "Lorena"));
 
 //Exercise Monday nov. 4th
 
-const target = Math.floor(Math.random () * 10);
-let guess = null;
+/*// EXERCISE 1: Guess the random number 
 
-console.log(target);
+Create a function guessNumberGame that generates a random number between 1 and 10. 
+
+The function should repeatedly prompt the user to guess the number until the correct number is guessed. 
+
+Use a while loop to keep asking the user for input until they guess correctly.  
+
+Provide feedback if the guess is too high or too low. 
+
+ 
+
+ 
+
+// EXERCISE 2: Create an array of random numbers 
+
+Create a function generateRandomArray that accepts one argument: length (the length of the array). 
+
+Inside the function, use a for loop to generate an array of random numbers between 1 and 100. 
+
+Return the array from the function. 
+
+ 
+
+ 
+
+// EXERCISE 3: Calculate the sum of even numbers in the array 
+
+Create a function sumEvenNumbers that accepts an array as an argument. 
+
+Use a for loop to iterate through the array*/
+
+function guessNumber() {
+
+const targetNumber = Math.floor(Math.random () * 10) + 1;
+let guess = false;
+
+
+
+while (guess !== targetNumber) {
+    guess = parseInt(prompt("Guess a number between 1 a 10: "), 10);
+    
+    if (isNaN(guess)) {
+        alert("Please enter a valid number.");
+        console.log ("Invalid number: " + guess, "try again");
+    } else if (guess < targetNumber) {
+        alert("Too low! Try again.");
+        console.log ("Number guessed: " + guess, "try again");
+    } else if (guess > targetNumber) {
+        alert("Too high! Try again.");
+        console.log ("Number guessed: " + guess, "try again");
+    } else {
+        alert("Congratulations! You have guess the number!");
+        console.log ("Number guessed: " + guess, "try again");
+    }
+
+}
+
+}
+
+guessNumberGmae();
