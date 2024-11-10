bcd5a21ed69fe0016df270cd5a4c05910e2d76e2
Lorena Ojeda <lorena.ojeda.u@gmail.com>
2024-11-10 11:36:18 +0100

    ATM

--- main.js
+++ main.js
@@ -48,7 +48,6 @@ Inside the function, use a for loop to generate an array of random numbers betwe
 
 Return the array from the function. 
 
- 
 
  
 
@@ -57,8 +56,6 @@ Return the array from the function.
 Create a function sumEvenNumbers that accepts an array as an argument. 
 
 Use a for loop to iterate through the array*/
-
-
 
 
 //Guess the Number
@@ -92,7 +89,7 @@ guessNumberGame();
 
 //Generate an array of random numbers
 
-function generateRandomArray(length) {
+/*function generateRandomArray(length) {
 
   let randomArray = [];
 
@@ -108,4 +105,175 @@ function generateRandomArray(length) {
 console.log(generateRandomArray(8));
 console.log(generateRandomArray(5));
 console.log(generateRandomArray(12));
+*/
 
+/* Instructions: 
+
+BANK ACCOUNT 
+
+ 
+
+REQUIREMENTS 
+
+Create an object called account that has the following properties: 
+
+accountName, should be the data type string 
+
+This property should contain the name of the account holder 
+
+ 
+
+- balance, should be the data type number 
+
+this property should contain the total amount of the account 
+
+ 
+
+ - getBalance, should be a function 
+
+this function should display the total amount of the account to the user 
+
+ 
+
+ - deposit, also a function 
+
+ this function should be able to deposit money onto the balance of the account 
+
+ 
+
+ - withdrawal, also a function 
+
+ this function should be able do withdrawal money from the balance of the account 
+
+ 
+
+ - getAccountName, function as well 
+
+ this function should dispaly the account holders name to the user 
+
+ 
+
+ - accountError, same as above function! 
+
+ this one is a bit tricky... it's up to you to figure out how or what you should use this for. 
+
+ HINT: it's more a thinking poblem than a technical problem :) 
+
+ 
+
+EXTRA: exitAccount, should be a function 
+
+ this function should exit the account 
+
+ HINT: there are a few different ways to do this, it's up to you which way you choose. 
+
+ 
+
+ EXTRA = OPTIONAL NOT MANDATORY 
+
+ 
+
+ Remember that a function is just a value. And if a function is just a 
+
+ value then we can both pass it as a parameter to a function and 
+
+ pass it as a property of an object. 
+
+ 
+
+ The object should handle all of the functionality (logic) 
+
+ 
+
+ The atm() function should be responsible for showing the user interface 
+
+ and based on the user input show the right meny choice */
+
+
+
+const account = {
+                                                        
+    accountName: "Lorena Ojeda",                          
+     balance: 1000,
+
+                                                        
+  getBalance: function() {
+      console.log(`Your balance is: $${this.balance}`);
+  },
+
+                                                        
+  deposit: function(amount) {
+      if (amount > 0) {
+          this.balance += amount;
+          console.log(`Deposited $${amount}. New balance: $${this.balance}`);
+      } else {
+          this.accountError("Invalid deposit amount. Please enter again.");
+      }
+  },
+
+                                                        
+  withdrawal: function(amount) {
+      if (amount > 0 && amount <= this.balance) {
+          this.balance -= amount;
+          console.log(`Withdrew $${amount}. New balance: $${this.balance}`);
+      } else if (amount > this.balance) {
+          this.accountError("Insufficient balance for withdrawal.");
+      } else {
+          this.accountError("Invalid withdrawal amount. Please enter again.");
+      }
+  },
+
+                                                        
+  getAccountName: function() {
+      console.log(`Account Holder: ${this.accountName}`);
+  },
+
+                                                        
+  accountError: function(message) {
+      console.log(`Error: ${message}`);
+  },
+
+                                                        
+  exitAccount: function() {
+      console.log("Exiting the account...");
+      this.accountName = null;
+      this.balance = 0;
+  }
+};
+
+                                                       
+function atm() {
+  let action;
+  do {
+      action = prompt("Check your account: \n1. View Balance \n2. Deposit \n3. Withdraw \n4. View Account Name \n5. Exit Account \n6. Quit");
+
+      switch (action) {
+          case "1":
+              account.getBalance();
+              break;
+          case "2":
+              const depositAmount = parseFloat(prompt("Enter amount to deposit:"));
+              account.deposit(depositAmount);
+              break;
+          case "3":
+              const withdrawalAmount = parseFloat(prompt("Enter amount to withdraw:"));
+              account.withdrawal(withdrawalAmount);
+              break;
+          case "4":
+              account.getAccountName();
+              break;
+          case "5":
+              account.exitAccount();
+              console.log("Account exited.");
+              action = "6"; 
+              break;
+          case "6":
+              console.log("Thank you for using our ATM!");
+              break;
+          default:
+              console.log("Invalid choice. Please try again.");
+      }
+  } while (action !== "6");
+}
+
+atm();
